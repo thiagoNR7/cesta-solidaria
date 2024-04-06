fac8ecd5bb5bd53d5197955b477fbc97939f2b49
Thiago NR <thiagokroos55gmail.com>
2024-04-05 23:06:08 -0300

    finalizando checkbox

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,10 +1,22 @@
-import { Component, OnInit, } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Menu } from 'src/app/interfaces/menu.interfaces';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
-export class MenuComponent{
+export class MenuComponent {
+  isMenuOpen = false;
+  menuPainel: Menu = {
+    title: 'painel Administrativo',
+    path: '/login',
+  };
+  menus = [this.menuPainel];
+ 
 
+  closeOpenMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    console.log(this.isMenuOpen);
   }
+}
